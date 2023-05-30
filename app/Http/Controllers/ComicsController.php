d46a9d044fcd247f5e3cddea8dd2b01c2f34a488
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-05-30 15:28:57 +0200

    Add: delete function

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -99,6 +99,8 @@ class ComicsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+        return redirect()->route('comics.index');
     }
 }
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,17 +7,29 @@ I tuoi fumetti
     <h1 class="text-center text-uppercase pb-3">Il tuo archivio</h1>
     <div class="row d-flex">
         @foreach ($comics as $comic)
-            <div class="card col-3 d-flex p-2 ms_card mb-3">
+            <div class="card col-3 d-flex p-4 ms_card mb-3">
                 <img src="{{ $comic->thumb }}" class="card-img-top ms_card-img" alt="...">
                 <div class="card-body">
                     <div class="ms_titles">
                         <h3 class="text-center">{{ $comic->title }}</h3>
                         <h5 class="text-center">{{ $comic->series }}</h5>
                     </div>
-                <p class="card-text ms_description">{{ $comic->description }}</p>
-                <a href="{{ Route('comics.show', $comic->id) }}" class="btn btn-primary mb-3">Clicca qui per saperne di più</a>
-                <a href="{{ Route('comics.edit', $comic->id) }}" class="btn btn-warning mb-5">Modifica questo fumetto</a>
-            </div>
+                    <p class="card-text ms_description">{{ $comic->description }}</p>
+                    <div class="text-center">
+                        <a href="{{ Route('comics.show', $comic->id) }}" class="btn btn-primary mb-1">Clicca qui per saperne di più</a>
+                    </div>
+                    <div class="text-center">
+                        <a href="{{ Route('comics.edit', $comic->id) }}" class="btn btn-warning mb-1">Modifica questo fumetto</a>
+                    </div>
+                    <div class="text-center">
+                        <form action="{{ Route('comics.destroy', $comic->id) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-danger mb-5">Elimina questo fumetto</button>
+        
+                        </form>
+                    </div>
+                </div>
             </div>
             
         @endforeach
