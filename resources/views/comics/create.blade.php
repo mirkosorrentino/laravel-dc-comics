08d8b0367cbc3570f2ece910ac354962f4656206
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-06-01 15:20:32 +0200

    Add: validation through @errors->any() in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,6 +6,13 @@
 
 @section('content')
 <div class="container">
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
     <form action="{{ route('comics.store') }}" method="POST">
         @csrf
         <div class="mb-3">
