7d36f11b30a6460cfa9891b0cb6f2970a610422b
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-06-02 16:24:17 +0200

    Add: custom error message on every input

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -33,4 +33,24 @@ class StoreComicRequest extends FormRequest
             'sale_date' => 'required|date_format:Y-m-d',
         ];
     }
+
+    public function messages() {
+        return [
+            'type.required' => "E' obbligatorio inserire il tipo di fumetto",
+            'type.max' => 'Il massimo di caratteri è :max',
+            'title.required' => "E' obbligatorio inserire il titolo del fumetto",
+            'title.min' => 'Il minimo di caratteri è :min',
+            'title.max' => 'Il massimo di caratteri è :max',
+            'series.required' => "E' obbligatorio inserire l'edizione",
+            'series.min' => 'Il minimo di caratteri è :min',
+            'series.max' => 'Il massimo di caratteri è :max',
+            'description.required' => "E' obbligatorio inserire una descrizione del fumetto",
+            'thumb.required' => "E' obbligatorio inserire un immagine",
+            'price.required' => "E' obbligatorio inserire un prezzo",
+            'price.min' => "I caratteri inseriti non sono sufficienti per stabilire un prezzo",
+            'price.max' => "Il numero di caratteri che puoi inserire è :max",
+            'sale_date.required' => "E' obbligatorio inserire la data di uscita",
+            'sale_date.date_format' => "Il format deve essere YYYY-MM-DD",
+        ];
+    }
 }
