4aff24ededf57f200bd285c76e7213ef8693d7b4
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-06-02 16:45:25 +0200

    Add: custom error message in edit.blade.php, Job Completed

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -22,38 +22,73 @@
 
         <div class="mb-3">
             <label for="type" class="form-label">Tipo di fumetto:</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{ old('type', $comic->type) }}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{ old('type', $comic->type) }}">
+            @error('type')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="title" class="form-label">Titolo:</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $comic->title) }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title', $comic->title) }}">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         
         <div class="mb-3">
             <label for="series" class="form-label">Edizione:</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{ old('series', $comic->series) }}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series', $comic->series) }}">
+            @error('series')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione:</label>
-            <input type="textarea" class="form-control" id="description" name="description" value="{{ old('description', $comic->description) }}">
+            <input type="textarea" class="form-control @error('description') is-invalid @enderror" id="description" name="description" value="{{ old('description', $comic->description) }}">
+            @error('description')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
         </div>
         
         <div class="mb-3">
             <label for="thumb" class="form-label">Immagine:</label>
-            <input type="text" class="form-control" id="thumb" name="thumb"  value="{{ old('thumb', $comic->thumb) }}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb"  value="{{ old('thumb', $comic->thumb) }}">
+            @error('thumb')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo:</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{ old('price', $comic->price) }}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price', $comic->price) }}">
+            @error('price')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Data di uscita:</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+            @error('sale_date')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Invia</button>
