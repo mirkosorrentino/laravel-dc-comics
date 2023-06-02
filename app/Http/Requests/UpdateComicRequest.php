fad191fc2d50fbb26ce2f0f62e58e94e45b85cd1
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-06-02 16:39:25 +0200

    Add: error message throught errors(any) in edit.blade

---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'type' => 'required | max:100',
+            'title' => 'required | min:5 | max:200',
+            'series' => 'required | min:5 | max:300',
+            'description' => 'required',
+            'thumb' => 'required',
+            'price' => 'required | min:2 | max:10',
+            'sale_date' => 'required|date_format:Y-m-d',
+        ];
+    }
+
+    public function messages() {
+        return [
+            'type.required' => "E' obbligatorio inserire il tipo di fumetto",
+            'type.max' => 'Il massimo di caratteri è :max',
+            'title.required' => "E' obbligatorio inserire il titolo del fumetto",
+            'title.min' => 'Il minimo di caratteri è :min',
+            'title.max' => 'Il massimo di caratteri è :max',
+            'series.required' => "E' obbligatorio inserire l'edizione",
+            'series.min' => 'Il minimo di caratteri è :min',
+            'series.max' => 'Il massimo di caratteri è :max',
+            'description.required' => "E' obbligatorio inserire una descrizione del fumetto",
+            'thumb.required' => "E' obbligatorio inserire un immagine",
+            'price.required' => "E' obbligatorio inserire un prezzo",
+            'price.min' => "I caratteri inseriti non sono sufficienti per stabilire un prezzo",
+            'price.max' => "Il numero di caratteri che puoi inserire è :max",
+            'sale_date.required' => "E' obbligatorio inserire la data di uscita",
+            'sale_date.date_format' => "Il format deve essere YYYY-MM-DD",
+        ];
+}
+}
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -8,6 +8,13 @@
 <div class="container">
 
     <h2>Modifica il fumetto: {{ $comic->title }}</h2>
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
     <form action="{{ route('comics.update', $comic->id) }}" method="POST">
         @method('PUT')
@@ -15,38 +22,38 @@
 
         <div class="mb-3">
             <label for="type" class="form-label">Tipo di fumetto:</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}">
+            <input type="text" class="form-control" id="type" name="type" value="{{ old('type', $comic->type) }}">
         </div>
 
         <div class="mb-3">
             <label for="title" class="form-label">Titolo:</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $comic->title) }}">
         </div>
 
         
         <div class="mb-3">
             <label for="series" class="form-label">Edizione:</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+            <input type="text" class="form-control" id="series" name="series" value="{{ old('series', $comic->series) }}">
         </div>
         
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione:</label>
-            <input type="textarea" class="form-control" id="description" name="description" value="{{ $comic->description }}">
+            <input type="textarea" class="form-control" id="description" name="description" value="{{ old('description', $comic->description) }}">
         </div>
         
         <div class="mb-3">
             <label for="thumb" class="form-label">Immagine:</label>
-            <input type="text" class="form-control" id="thumb" name="thumb"  value="{{ $comic->thumb }}">
+            <input type="text" class="form-control" id="thumb" name="thumb"  value="{{ old('thumb', $comic->thumb) }}">
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo:</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+            <input type="text" class="form-control" id="price" name="price" value="{{ old('price', $comic->price) }}">
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Data di uscita:</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
         </div>
 
         <button type="submit" class="btn btn-primary">Invia</button>
